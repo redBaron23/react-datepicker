5b41516e24e3bd8512cfdeca34a0a02399225d56
Pato <patotomastoledo6@gmail.com>
2022-04-21 11:13:08 -0300

    without lib

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import { DateTimePickerActions, DateTimePickerProps } from './types';
+export declare const DateTimePicker: React.ForwardRefExoticComponent<DateTimePickerProps & React.RefAttributes<DateTimePickerActions>>;
+export default DateTimePicker;
---
+++ types.d.ts
@@ -0,0 +1,14 @@
+type EventData = {
+    id: string;
+    date: Date;
+    email: string;
+}
+
+export declare type DateTimePickerActions = {
+
+};
+
+export interface DateTimePickerProps {
+    eventsData: EventData[];
+}
+
